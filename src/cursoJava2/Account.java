7fc93fc6abe01822f01d263a18f097cd2628955e
danilosheen <c.danilo.f.silva@gmail.com>
2019-12-05 21:43:29 -0300

    add new exemple

---
+++ src/cursoJava2/Account.java
@@ -0,0 +1,55 @@
+package cursoJava2;
+
+public class Account {
+	
+	private int number;
+	private String holder;
+	private double balance;
+	
+	//Construtores
+	
+	public Account(int number, String holder) {
+		this.number = number;
+		this.holder = holder;
+	}
+
+
+	public Account(int number, String holder, double initialDeposit) {
+		this.number = number;
+		this.holder = holder;
+		deposit(initialDeposit);
+	}
+	
+	//metodos de acesso
+	
+	public String getHolder() {
+		return holder;
+	}
+
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+
+	public int getNumber() {
+		return number;
+	}
+
+
+	public double getBalance() {
+		return balance;
+	}	
+	
+	// metodos
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount + 5.0;
+	}
+		
+
+}
--- src/cursoJava2/Main.java
+++ src/cursoJava2/Main.java
@@ -1,9 +1,18 @@
 package cursoJava2;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		
+		
+		sc.close();
 	}
 
 }
